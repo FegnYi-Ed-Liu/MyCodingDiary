8a5101a1ce88b4d6996e39f22fbc3a0839c443f0
Feng-Yi Liu <edsolitude@gmail.com>
2021-03-12 21:05:10 -0500

    still tes

---
+++ Day10.py
@@ -0,0 +1,6 @@
+
+from selenium import webdriver
+
+url = 'https://www.facebook.com/Crossing.cw/videos/2618864651566880'
+driver = webdriver.Chrome("/Users/edliu/Documents/chrom_driver/chromedriver")
+driver.get(url)
--- Day8.py
+++ Day8.py
@@ -1,8 +1,19 @@
 # 0304 buildup one job search and hunting dictionary data
-from typing import Dict, List, Union
+# from typing import Dict, List, Union
+#
+# 第一步
+# 寫出一個class 可以記錄公司名稱 位置 薪水 需要技能
+# 存成dictionary in text file.
 
 import pandas as pd
 import numpy as np
+
+dict_sample = {
+    "Company": "Toyota",
+    "position": "data scientist",
+    "salary": 120000,
+    "skill": ["Python", "R", "AI"]
+}
 
 
 class job_search:
@@ -12,16 +23,24 @@ class job_search:
         self.position = position
         self.salary = salary
         self.skill = skill
+        self.dicc = {'company': company, 'position': position, 'salary': salary, 'skill': skill}
 
-    def dic_db(self) -> object:
-        for k, v in self.items():
-            print(k, v)
+    def save_dic(self):
+        for key in self.dicc:
+            print(key)
+        for item in self.dicc.items():
+            print(item)
+        return self.dicc
+
+    # dic_db(self) -> object:
+    #
+    # filename = 'jobsearch.txt'
+    #     with open(filename. 'w') as file_object:
+    #         note_content = file_object.write({'company': self.company,'position': self.position,'salary': self.salary,'skill': self.skill,})
 
 
-def main():
-    dict_sample = {
-        "Company": "Toyota",
-        "position": "data scientist",
-        "salary": 120000,
-        "skill": ["Python", "R", "AI"]
-    }
+if __name__ == '__main__':
+    test_1 = job_search('tes', 'www', 2222, 'test')
+
+tessss = job_search.save_dic(test_1)
+print(tessss)
